a12285f448c3697ef3dbe8147faca453edfb2b5a
Aarav singh <singhaarav059@gmail.com>
2018-08-03 19:25:11 +0530

    Update hardware.mk

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,12 +1,10 @@
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
 
-<<<<<<< HEAD
 # Lineage hardware
 BOARD_HARDWARE_CLASS += \
     device/lenovo/A6020/lineagehw
-=======
+
 #DoubleTapToWake support
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
->>>>>>> 6ea75a7... A6020: Set double tap to wake node
