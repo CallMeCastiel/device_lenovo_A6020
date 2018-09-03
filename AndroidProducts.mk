9c0a937dd85fb9ef5c152fd43d0571fdc3811f86
root <root@winnie.c.winnie-213914.internal>
2018-09-03 00:16:59 +0000

    Revert "A6020: AEX bringup"
    This reverts commit a6555b1f6dc141906f11d73df54561f0f90ff3c4.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,8 @@
 PRODUCT_MAKEFILES := \
+<<<<<<< HEAD
     $(LOCAL_DIR)/full_A6020.mk \
     $(LOCAL_DIR)/dnd_A6020.mk \
 
+=======
+    $(LOCAL_DIR)/full_A6020.mk
+>>>>>>> parent of a6555b1... A6020: AEX bringup
--- doze/Android.mk
+++ doze/Android.mk
@@ -8,6 +8,28 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := LenovoDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+<<<<<<< HEAD
+=======
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v7-appcompat \
+    android-support-v14-preference \
+    org.cyanogenmod.platform.internal
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
+>>>>>>> parent of a6555b1... A6020: AEX bringup
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
